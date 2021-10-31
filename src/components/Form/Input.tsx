b3458bad1a4c7f01313c02a7b654b154236db7d8
marcos <marcos@grana.capital>
2021-10-30 22:42:02 -0300

    first part and almost second part

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,35 @@
+import {
+  Flex,
+  Input as ChakraInput,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        id="email"
+        name="email"
+        type="email"
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
